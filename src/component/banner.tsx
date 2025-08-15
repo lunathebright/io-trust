13ba17800b2c8afc598e4495934c16219f6516ab
Danielle <lunabright817@gmail.com>
2025-08-15 16:08:32 +0900

    [ADD] 초기 세팅

---
+++ src/component/banner.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Banner() {
+  return <div className="border border-red-500">Banner</div>;
+}
---
+++ src/component/service.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Service() {
+  return <div className="border border-green-400">Service</div>;
+}
